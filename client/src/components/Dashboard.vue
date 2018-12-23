aee838b224954b99ace6a6a5b5c5479a68f3ad8b
Pawel Peczkowski <peczkowskip@gmail.com>
2018-12-23 19:07:47 +0100

    Install pug and sass loaders and clean template from basic setup

---
+++ client/src/components/Dashboard.vue
@@ -0,0 +1,20 @@
+<template lang="pug">
+	v-container(fluid)
+		v-layout(row wrap)
+			v-flex(xs12)
+				
+
+
+</template>
+
+<script>
+  export default {
+    data: () => {
+		
+    }
+  }
+</script>
+
+<style scoped lang="sass">
+
+</style>
