5c662c75f8662aee6abb06ff3e6be1298326caaa
Pawel Peczkowski <peczkowskip@gmail.com>
2018-12-24 16:12:26 +0100

    InputProfile component has been created

---
+++ client/src/components/modules/InputProfile.vue
@@ -0,0 +1,34 @@
+<template lang="pug">
+    v-layout(row wrap)
+        v-flex(xs8)
+            v-text-field(v-model="inputProfile" label="Enter a nickname")
+        v-flex(xs4)
+            v-btn( color="info") Get info
+                v-icon(dark right v-text="'$vuetify.icons.user'")
+
+</template>
+<script>
+    export default {
+        props: {
+            value: {
+                type: String,
+                default: ''
+            }
+        },
+        data () {
+            return {
+                inputProfile: this.value
+            }
+        },
+        watch: {
+            inputProfile: {
+                handler (value) {
+                    this.$emit('input', value)
+                }
+            }
+        }
+    }
+</script>
+
+<style scoped>
+</style>
