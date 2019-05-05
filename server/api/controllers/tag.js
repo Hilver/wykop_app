f8d4f8a1cc8df16bcf4bd9e999207e2b0a6da039
Pawel Peczkowski <peczkowskip@gmail.com>
2019-05-05 18:45:10 +0200

    [API] Tag route has been implemented

--- server/api/controllers/tag.js
+++ server/api/controllers/tag.js
@@ -23,3 +23,23 @@ exports.get_tag_info = (req, res) => {
 			res.status(404).json({err})
 		})
 }
+
+exports.get_popular_tags = (req, res) => {
+	const popularTags = `https://a.wykop.pl/Tags/Index/appkey/${process.env.APP_KEY}`
+	const params = {
+		method: 'GET',
+		headers: {
+			'apisign': apisign(popularTags)
+		}
+	}
+
+	fetch(popularTags, params)
+		.then(data => data.json())
+		.then(result => {
+            res.status(201).json(result)
+            console.log(result)
+		})
+		.catch(err => {
+			res.status(404).json({err})
+		})
+}
--- server/api/routes/tag.js
+++ server/api/routes/tag.js
@@ -5,4 +5,6 @@ const tagControllers = require('../controllers/tag.js')
 
 router.get('/:tag', tagControllers.get_tag_info)
 
+router.get('/popular-tags', tagControllers.get_popular_tags)
+
 module.exports = router
