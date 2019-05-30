e88d66f86be45b3feb1b13f9fe1a31b6d41b9a79
Pawel Peczkowski <peczkowskip@gmail.com>
2019-05-30 17:20:26 +0200

    [General] Security maintenance

--- server/api/models/users.js
+++ server/api/models/users.js
@@ -1,20 +1,20 @@
 const bookshelf = require('../config/bookshelf')
 
 const User = bookshelf.Model.extend({
-    tableName: 'users_stats'
+	tableName: 'users_stats'
 })
 
 module.exports.create = user => {
 	return new User({
 		user_id: user.id,
-        user_nickname: user.nickname,
-        links_added_count: user.links_added_count,
-        links_published_count: user.links_published_count,
-        comments_added: user.comments_added,
-        diggs: user.diggs,
-        effectiveness: user.effectiveness,
-        avg_daily_digg: user.avg_daily_digg,
-        followers: user.followers,
-        photoUrl: user.photoUrl
+		user_nickname: user.nickname,
+		links_added_count: user.links_added_count,
+		links_published_count: user.links_published_count,
+		comments_added: user.comments_added,
+		diggs: user.diggs,
+		effectiveness: user.effectiveness,
+		avg_daily_digg: user.avg_daily_digg,
+		followers: user.followers,
+		photoUrl: user.photoUrl
 	}).save()
 }
--- server/app.js
+++ server/app.js
@@ -7,14 +7,14 @@ app.use(express.json())
 app.use(express.urlencoded({extended: false}))
 
 app.use((req, res, next) => {
-        res.header('Access-Control-Allow-Origin', '*'); // * - Allow access to any client
-        res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
-        if(req.method === 'OPTIONS') {
-            res.header('Access-Control-Allow-Methods', 'PUT, POST, PATCH, DELETE, GET');
-            return res.status(200).json({});
-        }
-        next();
-    })
+		res.header('Access-Control-Allow-Origin', '*'); // * - Allow access to any client
+		res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
+		if(req.method === 'OPTIONS') {
+			res.header('Access-Control-Allow-Methods', 'PUT, POST, PATCH, DELETE, GET');
+			return res.status(200).json({});
+		}
+		next();
+	})
 
 const profileRoutes = require('./api/routes/profile')
 const topRoutes = require('./api/routes/top')
