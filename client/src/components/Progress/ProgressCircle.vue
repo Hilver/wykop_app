41f2225c4ed3abdb9b9cc21b43ee9d9c298beae3
Pawel Peczkowski <peczkowskip@gmail.com>
2019-06-25 22:39:25 +0200

    [App] Circular Progress has been implemented into Profile component

---
+++ client/src/components/Progress/ProgressCircle.vue
@@ -0,0 +1,10 @@
+<template lang="pug">
+	v-container(fill-height)
+		v-layout(row justify-center align-center)
+			v-progress-circular(:size="50" color="primary" indeterminate)
+</template>
+<script>
+export default {
+	name: 'CircularProgress'
+}
+</script>
